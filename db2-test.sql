784a5152da8f26718c84e9a0e62e417e0f96942d
unknown <>
2020-12-26 00:33:22 +0100

    first commit db2-test.sql

---
+++ db2-test.sql
@@ -0,0 +1,27 @@
+#creating DATABASE
+
+CREATE DATABASE 'movie_management';
+
+USE 'movie_management';
+
+CREATE TABLE 'Studios' (
+  studio_id NUMBER PRIMARY_KEY,
+  name CHAR(20) UNIQUE,
+  city VARCHAR2(50),
+  state CHAR(2)
+)
+
+CREATE TABLE 'Movies' (
+  movie_title VARCHAR2(40),
+  studio_id NUMBER REFERENCES Studios(studio_id)
+)
+
+CREATE TABLE 'Showtimes' (
+  theater_name VARCHAR2(50),
+  screen CHAR(30),
+  showing DATE,
+  movie_title VARCHAR2(40),
+  studio_id NUMBER,
+  FOREIGN_KEY (theater_name, screen, showing),
+  (movie_title, studio_id) REFERENCES Movies(movie_title, studio_id);
+)
